1343b7c83bebe787b848a0b3561ca45467b296fc
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-04-25 13:44:55 +0000

    react

--- 03.project/project/src/App.jsx
+++ 03.project/project/src/App.jsx
@@ -1,14 +1,23 @@
 import "./App.css";
-
+import AppName from "./component/component";
+import "AddTodo"
 function App() {
   return (
-    <center class="container">
-      <h1>todo app</h1>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6"><input type="text" placeholder="Enter TODO Here" /></div>
-          <div class="col-4"><input type="date" name="" id="date" /></div>
-          <div class="col-2">col-4</div>
+    <center className="container">
+      <AppName></AppName>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col-6">
+            <input type="text" placeholder="Enter TODO Here" />
+          </div>
+          <div className="col-4">
+            <input type="date" name="" id="date" />
+          </div>
+          <div className="col-2">
+            <button type="button" className="btn btn-outline-success">
+              Add
+            </button>
+          </div>
         </div>
       </div>
     </center>
